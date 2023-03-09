f429a04f5d7414fd06702a7347089c5be1b2955b
CL <75270770+ongcl03@users.noreply.github.com>
2023-03-10 03:58:54 +0800

    improve the code with good practice
    1. improve the code with good practice
    2. fix "\n" bug in output by using replace "\\n" to "\n"
    3. add a throw catch exception to handle error

--- ChatGPT.java
+++ ChatGPT.java
@@ -1,27 +1,34 @@
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-
 
 public class HttpRequestMultipleHeadersExample {
 
-    public static void main(String[] args) throws Exception {
-        while (true){
-            chatWithGPT();
+    private static final String OPENAI_URL = "https://api.openai.com/v1/chat/completions";
+    private static final String AUTH_TOKEN = "Bearer sk-......";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("😋: ");
+            String question = scanner.nextLine();
+            if (question.equals("exit")) {
+                scanner.close();
+                return;
+            }
+            try {
+                chatWithGPT(question);
+            } catch (Exception e) {
+                System.err.println("Error occurred: " + e.getMessage());
+            }
         }
-        
     }
 
-    public static void chatWithGPT() throws Exception{
-        String url = "https://api.openai.com/v1/chat/completions";
-        URL obj = new URL(url);
+    public static void chatWithGPT(String question) throws Exception{
+        URL obj = new URL(OPENAI_URL);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
         // set request method
@@ -29,36 +36,26 @@ public class HttpRequestMultipleHeadersExample {
 
         // set custom headers
         con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("Authorization", "Bearer sk-......");
-
-
-         // create a Scanner object to read input from the console
-         Scanner scanner = new Scanner(System.in);
-
-         // prompt the user to enter a string
-         System.out.print("😋: ");
- 
-         // read the user's input as a string
-         String question = scanner.nextLine();
+        con.setRequestProperty("Authorization", AUTH_TOKEN);
 
         // set request body
         String requestBody = "{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \"" + question + "\"}]}";
         con.setDoOutput(true);
-        DataOutputStream out = new DataOutputStream(con.getOutputStream());
-        out.writeBytes(requestBody);
-        out.flush();
-        out.close();
+        try (DataOutputStream out = new DataOutputStream(con.getOutputStream())) {
+            out.write(requestBody.getBytes());
+            out.flush();
+        }
 
         int responseCode = con.getResponseCode();
         // System.out.println("Response Code : " + responseCode);
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        StringBuilder response = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
         }
-        in.close();
 
         // System.out.println("Response Body : " + response.toString());
 
@@ -67,11 +64,9 @@ public class HttpRequestMultipleHeadersExample {
         String jsonString = response.toString();
         int contentIndex = jsonString.indexOf("\"content\":");
         String contentValue = jsonString.substring(contentIndex + "\"content\":".length() + 1, jsonString.indexOf("\"", contentIndex + "\"content\":".length() + 2));
-        System.out.print("🤖: " + contentValue);
+        contentValue = contentValue.replace("\\n", "\n"); // replace all occurrences of \n with actual newline character
+        contentValue = contentValue.replaceAll("^(\\n)+", ""); // remove all leading newline characters
+        System.out.println("🤖: " + contentValue);
         System.out.println("\n");
     }
-
-
-
-   
 }
